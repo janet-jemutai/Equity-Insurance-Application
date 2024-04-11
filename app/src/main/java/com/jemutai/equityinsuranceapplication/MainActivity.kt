43019230074b7f4aabfa355776f5aaab9da0f7b6
Norpha <normanndiritu@gmail.com>
2024-04-11 11:01:33 +0300

    Updated Policies

--- app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
+++ app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
@@ -3,13 +3,21 @@ package com.jemutai.equityinsuranceapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jemutai.equityinsuranceapplication.ui.theme.EquityInsuranceApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
     }
+
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EquityInsuranceApplicationTheme {
-        Greeting("Android")
-    }
+        Column(
+            modifier = Modifier.fillMaxSize()
+
+        ) {
+            Policies()
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
 }
