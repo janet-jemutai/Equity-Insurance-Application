828c9d78ad704d7d6355227bc4f2df043eb56147
dominickiplimo <dominickiplimo2017@gmail.com>
2024-04-11 15:58:12 +0300

    Revert "Bottom NavBar"
    This reverts commit f8f6e4d9dea455eebaa88cf03e51630a9eef72ef.

--- app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
+++ app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
@@ -9,10 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.jemutai.equityinsuranceapplication.navigation.AppNavigation
-import com.jemutai.equityinsuranceapplication.ui.theme.CustomColors
 import com.jemutai.equityinsuranceapplication.ui.theme.EquityInsuranceApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +17,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EquityInsuranceApplicationTheme {
-
-AppNavigation()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
             }
         }
+    }
+}
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EquityInsuranceApplicationTheme {
+        Greeting("Android")
     }
 }
