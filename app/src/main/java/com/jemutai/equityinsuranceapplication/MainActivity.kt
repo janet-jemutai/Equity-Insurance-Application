f034807ebdaf206da16d958440fb9fda02cb6b8c
dominickiplimo <dominickiplimo2017@gmail.com>
2024-04-11 12:20:55 +0300

    second commit
    Added Claims section

--- app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
+++ app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.jemutai.equityinsuranceapplication.ui.theme.EquityInsuranceApplicationTheme
 
 class MainActivity : ComponentActivity() {
