559af54716d2568aaacf05e70b25636d1a4811cf
Dominic Kiplimo <74908135+dommy98@users.noreply.github.com>
2024-04-11 11:56:15 +0300

    Revert "claims section"

--- app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
+++ app/src/main/java/com/jemutai/equityinsuranceapplication/MainActivity.kt
@@ -3,20 +3,14 @@ package com.jemutai.equityinsuranceapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.jemutai.equityinsuranceapplication.navigation.AppNavigation
 import com.jemutai.equityinsuranceapplication.ui.theme.CustomColors
 import com.jemutai.equityinsuranceapplication.ui.theme.EquityInsuranceApplicationTheme
@@ -26,29 +20,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EquityInsuranceApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    HomeScreen()
-                }
 
 AppNavigation()
             }
         }
 
-
-@Preview
-@Composable
-fun HomeScreen() {
-    Scaffold { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-        ) {
-            ClaimsSection()
-        }
     }
 }
--- app/src/main/java/com/jemutai/equityinsuranceapplication/ui/theme/Color.kt
+++ app/src/main/java/com/jemutai/equityinsuranceapplication/ui/theme/Color.kt
@@ -10,6 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val maroon = Color(0xFF800000)
 val Maroon =Color(0xFF800000)
 
